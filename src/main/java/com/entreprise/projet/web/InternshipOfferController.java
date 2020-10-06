fedb8894b6bdd59c84be0df4d398a9c11bd790f6
llyasstrh <ilyasstrh@gmail.com>
2020-10-06 16:18:39 +0100

    Add an inernship offer

--- src/main/java/com/entreprise/projet/web/InternshipOfferController.java
+++ src/main/java/com/entreprise/projet/web/InternshipOfferController.java
@@ -2,6 +2,8 @@ package com.entreprise.projet.web;
 
 import com.entreprise.projet.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,10 +37,33 @@ public class InternshipOfferController {
 
     //Add an inernship offer
     @PostMapping("/internshipoffers")
-    public InternshipOffer addInternshipOffer(@RequestBody InternshipOffer internshipOffer) {
-        //internshipOffer.setStudent(studentRepo.save(internshipOffer.getStudent()));
+    public InternshipOffer addInternshipOffer(@RequestParam(value = "studentId") Long studentId,
+                                              @RequestBody InternshipOffer internshipOffer
+                                              ) {
+        Student studentX = studentRepo.findById(studentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found :: " + studentId));
+        studentX.setId(studentId);
+        //studentX.setUsername(student.getUsername());
+        //studentX.setEmail(student.getEmail());
+        internshipOffer.setStudent(studentX);
         return internshipOfferRepo.save(internshipOffer);
+        //return ResponseEntity.ok(internshipOffer);
     }
+    /*
+    //Add an inernship offer
+    @PostMapping("/internshipoffers/student/{studentId}")
+    public InternshipOffer addInternshipOffer(@PathVariable(value = "id") Long studentId,
+                                              @RequestBody InternshipOffer internshipOffer) {
+        Student student = studentRepo.findById(studentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Instructor not found :: " + instructorId));
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setEmail(userDetails.getEmail());
+        final Instructor updatedUser = instructorRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+    */
+
 
     //delete an internship offer
     @DeleteMapping("/internshipoffers/delete/{id}")
