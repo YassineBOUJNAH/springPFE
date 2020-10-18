843b0a4520a7b2e70f8fb8d2a9cf280a78fbea81
Yassine Boujnah <yassineboujnah@gmail.com>
2020-10-18 01:06:15 +0100

    add mysql online database

--- src/main/java/com/entreprise/projet/web/InternshipController.java
+++ src/main/java/com/entreprise/projet/web/InternshipController.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-
+@CrossOrigin("*")
 @RestController
 public class InternshipController {
 
